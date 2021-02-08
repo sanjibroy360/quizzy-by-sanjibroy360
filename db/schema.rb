b7019c42cdacf8f37d9755c26c41a72b4fecf155
sanjibroy360 <sanjibroy0098@gmail.com>
2021-02-08 18:58:42 +0530

    Add unit test for user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_090458) do
+ActiveRecord::Schema.define(version: 2021_02_08_103128) do
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,86 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: "Tony", last_name: "Stark", email: "tony360@gmail.com")
+  end
+
+  def test_user_should_be_valid
+    assert @user.valid?
+  end
+
+  def test_first_name_should_be_present
+    @user.first_name = "     "
+    assert_not @user.valid?
+  end
+
+  def test_last_name_should_be_present
+    @user.last_name = "     "
+    assert_not @user.valid?
+  end
+
+  def test_email_should_be_present
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  def test_first_name_should_not_be_too_long
+    @user.first_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  def test_last_name_should_not_be_too_long
+    @user.last_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  def test_email_should_be_unique
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  def test_email_should_be_saved_as_lowercase
+    mixed_case_email = "tOnY360@gmail.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  def test_invalid_email_should_not_be_accepted
+    invalid_emai = "tony360@gmail,com"
+    @user.email = invalid_emai
+    assert_not @user.valid?
+  end
+
+  def test_valid_email_should_be_accepted
+    @user.email = "hello4world007@gmail.com"
+    assert @user.valid?
+  end
+
+  def test_email_should_not_be_case_sensitive
+    uppercase_email = "SAM@example.com"
+    lowercase_email = uppercase_email.downcase
+
+    duplicate_user = User.new(first_name: "Virat", last_name: "Kohli", email: lowercase_email)
+    @user.email = uppercase_email
+
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  def test_first_name_should_not_be_null
+    @user.first_name = nil
+    assert_not @user.valid?
+  end
+
+  def test_last_name_should_not_be_null
+    @user.last_name = nil
+    assert_not @user.valid?
+  end
+
+  def test_email_should_not_be_null
+    @user.email = nil
+    assert_not @user.valid?
+  end
 end
