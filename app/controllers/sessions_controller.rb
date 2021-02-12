3dbaf4c9c3687e90a78579b9f484f5901aa08029
sanjibroy360 <sanjibroy0098@gmail.com>
2021-02-12 17:32:58 +0530

    Fix the syntax error.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if (@user && @user.authenticate(params[:session][:password]))
       reset_session
       log_in @user
-      render json: { success: true, message: "Welcome, #{@user.first_name}!", user: @user,status: :ok
+      render json: { success: true, message: "Welcome, #{@user.first_name}!", user: @user},status: :ok
     else
       render json: { success: false, message: "Wrong email or password." }, status: 401
     end
