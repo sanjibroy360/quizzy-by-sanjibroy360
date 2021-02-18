061f473d430f724037c7da06627f065c09231de7
sanjibroy360 <sanjibroy0098@gmail.com>
2021-02-18 13:11:49 +0530

    Add unit tests for Quiz model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   validates :role, presence: true
   validates :email, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
